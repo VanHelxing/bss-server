246fb0f6166ea458fa8364691e5c6b8157a5d0b2
yangjunqing <yangjunqing@zhimadi.cn>
2018-06-13 13:42:39 +0800

    添加宝安松岗安监项目

--- bss-server/src/main/java/cn/zhimadi/bss/BssMainApplication.java
+++ bss-server/src/main/java/cn/zhimadi/bss/BssMainApplication.java
@@ -1,4 +1,12 @@
 package cn.zhimadi.bss;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BssMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BssMainApplication.class, args);
+    }
 }
